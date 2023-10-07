577f05d360dbf51473a434ff91e31f0b12ddc50d
itxTouseef74 <116633040+itxTouseef74@users.noreply.github.com>
2023-10-07 20:56:08 +0500

    Add updated code

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,37 +1,44 @@
 const userModel = require("../Model/userModel");
 
-const userController = async (req, res) => {
-  try {
+
+// next is defined for middleware if we dont define next than all commented functions should be used 
+// i have removed try catch in bcz now i have used express-asyn-error pkg by this this error provide try catch funtion itsel 
+const userController = async (req, res , next ) => {
+  // try {
     const { name, email, password } = req.body;
 
     if (!name) {
-      return res.status(400).send({
-        message: "Plz provide name",
-        success: false,
-        error,
-      });
+      next('Plz provide name')
+      // return res.status(400).send({
+      //   message: "Plz provide name",
+      //   success: false,
+      //   error,
+      // });
     }
     if (!email) {
-      return res.status(400).send({
-        message: "Plz provide email",
-        success: false,
-        error,
-      });
+      next('Plz provide email')
+      // return res.status(400).send({
+      //   message: "Plz provide email",
+      //   success: false,
+      //   error,
+      // });
     }
     if (!password) {
-      return res.status(400).send({
-        message: "Plz provide password",
-        success: false,
-        error,
-      });
+      next('Plz provide password')
+      // return res.status(400).send({
+      //   message: "Plz provide password",
+      //   success: false,
+      //   error,
+      // });
     }
 
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
-      return res.status(200).send({
-        message: "Email already exists plz login ",
-        success: false,
-      });
+      next('Email already exists plz login')
+      // return res.status(200).send({
+      //   message: "Email already exists plz login ",
+      //   success: false,
+      // });
     }
 
     const user = await userModel.create({ name, email, password });
@@ -40,14 +47,15 @@ const userController = async (req, res) => {
       success: true,
       user,
     });
-  } catch (error) {
+  // } catch (error) {
     console.log(error);
-    res.status(400).send({
-      message: "Error in Register controller",
-      success: false,
-      error,
-    });
+   next(error)
+    // res.status(400).send({
+    //   message: "Error in Register controller",
+    //   success: false,
+    //   error,
+    // });
   }
-};
+// };
 
 module.exports = userController;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const app=express();
+const express_async_error=require('express-async-errors')
 const dotEnv=require('dotenv');
 const colors=require('colors');
 const cors = require('cors');
 const morgan  = require ('morgan');
 const authRoutes=require('./Routes/authRoutes')
+const errMiddleware=require('./Middleware/errMiddleware')
+
 
 
 // config the dotenv 
@@ -21,6 +24,8 @@ const routes = require ('./Routes/routes')
 app.use('/api/v1/test' , routes)
 app.use('/api/v1/auth' , authRoutes)
 
+// validation middleware 
+app.use(errMiddleware);
 
 // Database 
 const connectDB=require('./Database/db');
